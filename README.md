0a50518acef8ebbe2efe55c26d10fd6d6f822ddd
RicoHorta <ricardo@peoplemuriae.com.br>
2023-03-11 23:08:41 -0300

    Inserir, Listar e evitar duplicata e-mail no MySql

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# crud-node" 
+Aplicação web responsiva de cadastro de clientes usando HTML - CSS - BootStrap - NODE.js - JavaScript - JSon - Express - HandleBars - SQL - MySQL - Sequelize - Xampp
--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.engine('hbs', hbs.engine({
     defaultLayout: 'main'
 })); app.set('view engine', 'hbs');
 
+app.use(express.static('public')); //MIDDLEWARE QUE DETERMINA QUAL A PASTA QUE CONTÉM OS NARQUIVOS ESTÁTICOS EX: CSS
+
+app.use(bodyParser.urlencoded({ extended: false })); //MIDDLEWARE HANDLE BODY-PARSER (POST) 
+
+
+//IMPORTAR MODEL USUARIO
+const Usuario = require('./models/Usuario');
+
 // Sessions Config
 
 app.use(session({
@@ -19,9 +27,6 @@ app.use(session({
     saveUninitialized: true
 }));
 
-//app.use(express.static('public')); MIDDLEWARE QUE DETERMINA QUAL A PASTA QUE CONTÉM OS NARQUIVOS ESTÁTICOS EX: CSS
-
-app.use(bodyParser.urlencoded({ extended: false })); //MIDDLEWARE HANDLE BODY-PARSER (POST) 
 
 //ROUTS
 app.get('/', (req, res) => {
@@ -36,9 +41,18 @@ app.get('/', (req, res) => {
     }
     res.render('index', { NavActiveCad: true });
 })
-
+//Na rota /users o Model Usuários invoca o método que faz tipo SELECT * e poe na variavel valores
 app.get('/users', (req, res) => {
-    res.render('users', { NavActiveUsers: true });
+    Usuario.findAll().then((valores) => {
+        if (valores.length > 0) {                                   //usuarios é enviado para users.hbs 
+            return res.render('users', { NavActiveUsers: true, table: true, usuarios: valores.map(valores => valores.toJSON()) });
+        } else {                     // NaActiveUsers->Cor do Botão,  tabela tem que aparecer, usuarios vai receber os valores do banco em JSon
+            res.render('users', { NavActiveUsers: true, table: false });
+        }
+    }).catch((err) => {
+        console.log(`Houve um erro: ${err}`);
+    });
+
 })
 
 app.get('/editar', (req, res) => {
@@ -60,32 +74,51 @@ app.post('/cad', (req, res) => {
     nome = nome.trim(); // remover espaços em branco antes e depois
     // Vericica se é vazio ou NULL
     if (nome == '' || typeof nome == 'undefined' || nome == null) {
-        erros.push('Campo Nome não pode ser vazio');
+        erros.push({ mensagem: "Campo Nome não pode ser vazio" });
     }
     // Verifica se é mesmo um nome
     if (!/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/.test(nome)) {
         erros.push({ mensagem: "Somente permitido letras e espaços em branco!" });
     }
     // Validação do email
+    email = email.toLowerCase(); //Passa para minúsculas
     email = email.trim(); // remover espaços em branco antes e depois
     if (email == '' || typeof email == 'undefined' || email == null) {
-        erros.push('Campo Email não pode ser vazio');
+        erros.push({ mensagem: "Campo Email não pode ser vazio" });
     }
     // Verificar se e-mail é válido com REGEX
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         erros.push({ mensagem: "Campo email inválido!" });
     }
+    // if (erros.length > 0) {
+    //     console.log(erros);
+    //     req.session.errors = erros;
+    //     req.session.success = false;
+    //     return res.redirect('/');
+    //}
+    //SUCESSO! (Nenhum erro)
+    // Salva os dados no MySql
+    //Usa o método create da model Usuario passando nome da coluna: nome da variável
+    Usuario.create({
+        nome: nome,
+        email: email,
+    }).then((usuario) => {
+        //console.log('success', usuario.toJSON());
+        req.session.success = true;
+        return res.redirect('/');
+    }).catch((erro) => {
+        //console.log(erro, email);
+        erros.push({ mensagem: "Usuário já cadastrado!" });
+        req.session.errors = erros;
+        req.session.success = false;
+        return res.redirect('/');
+    })
     if (erros.length > 0) {
         console.log(erros);
         req.session.errors = erros;
         req.session.success = false;
         return res.redirect('/');
     }
-    //SUCESSO! (Nenhum erro)
-    // Salva os dados no MySql
-    console.log('Validação OK!');
-    req.session.success = true;
-    return res.redirect('/');
 })
 
 app.listen(PORT, () => {
